b1555fc1a501673e642a7b97f09224068384e8f2
Anton Motin <anton.motin.dev@gmail.com>
2021-01-20 15:39:57 +0300

    Модели для статистики по нескольким домам

---
+++ xamarinJKH/Server/RequestModel/Monitor/RequestStatsArguments.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace xamarinJKH.Server.RequestModel.Monitor
+{
+    public class RequestStatsArguments
+    {
+        public List<RequestStatsQuerySettings> Queries { get; set; }
+    }
+}
