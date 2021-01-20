a86fbcc4759f5d0e4ca12db6ce066cca9480bef2
Anton Motin <anton.motin.dev@gmail.com>
2021-01-20 15:40:13 +0300

    Поле для ID группы районов

--- xamarinJKH/Server/RequestModel/NamedValue.cs
+++ xamarinJKH/Server/RequestModel/NamedValue.cs
@@ -7,6 +7,7 @@ namespace xamarinJKH.Server.RequestModel
     {
         public int ID { get; set; }
         public string Name { get; set; }
+        public int Value { get; set; }
 
         public string _Name
         {
