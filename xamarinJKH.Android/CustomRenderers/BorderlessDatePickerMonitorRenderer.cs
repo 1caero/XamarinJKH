6769c31f00598e6e197a92aadd588b300c562bda
Ilya Ponomarev <ipon85@yandex.com>
2021-01-22 12:45:32 +0300

    - исправил выделение фона нажатого элемента серым для списка заявок Жильца, убрал часть ненужного кода

--- xamarinJKH.Android/CustomRenderers/BorderlessDatePickerMonitorRenderer.cs
+++ xamarinJKH.Android/CustomRenderers/BorderlessDatePickerMonitorRenderer.cs
@@ -1,29 +1,29 @@
-﻿using Xamarin.Forms.Platform.Android;
-using Xamarin.Forms;
-using DatePicker = Xamarin.Forms.DatePicker;
-using xamarinJKH.Droid.CustomRenderers;
-using xamarinJKH;
+﻿//using Xamarin.Forms.Platform.Android;
+//using Xamarin.Forms;
+//using DatePicker = Xamarin.Forms.DatePicker;
+//using xamarinJKH.Droid.CustomRenderers;
+//using xamarinJKH;
 
-[assembly: ExportRenderer(typeof(BorderlessDatePickerMonitor), typeof(BorderlessDatePickerMonitorRenderer))]
-namespace xamarinJKH.Droid.CustomRenderers
-{
-    public class BorderlessDatePickerMonitorRenderer : DatePickerRenderer
-    {
-        public static void Init() { }
-        protected override void OnElementChanged(ElementChangedEventArgs<DatePicker> e)
-        {
-            base.OnElementChanged(e);
-            if (e.OldElement == null)
-            {
-                Control.Background = null;
+//[assembly: ExportRenderer(typeof(BorderlessDatePickerMonitor), typeof(BorderlessDatePickerMonitorRenderer))]
+//namespace xamarinJKH.Droid.CustomRenderers
+//{
+//    public class BorderlessDatePickerMonitorRenderer : DatePickerRenderer
+//    {
+//        public static void Init() { }
+//        protected override void OnElementChanged(ElementChangedEventArgs<DatePicker> e)
+//        {
+//            base.OnElementChanged(e);
+//            if (e.OldElement == null)
+//            {
+//                Control.Background = null;
 
-                var layoutParams = new MarginLayoutParams(Control.LayoutParameters);
-                layoutParams.SetMargins(0, 0, 0, 0);
-                LayoutParameters = layoutParams;
-                Control.LayoutParameters = layoutParams;
-                Control.SetPadding(0, 0, 0, 0);
-                SetPadding(0, 0, 0, 0);
-            }
-        }
-    }
-}
+//                var layoutParams = new MarginLayoutParams(Control.LayoutParameters);
+//                layoutParams.SetMargins(0, 0, 0, 0);
+//                LayoutParameters = layoutParams;
+//                Control.LayoutParameters = layoutParams;
+//                Control.SetPadding(0, 0, 0, 0);
+//                SetPadding(0, 0, 0, 0);
+//            }
+//        }
+//    }
+//}
