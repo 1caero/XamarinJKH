282562db8873e09ec681bf1069992fec7c0c8403
Ilya Ponomarev <ipon85@yandex.com>
2021-01-27 18:08:09 +0300

    - исправил значение по-умолчанию для выбора типа новой заявки, теперь по-умолчанию тип заявки не выбран.

--- xamarinJKH/AppResources.Designer.cs
+++ xamarinJKH/AppResources.Designer.cs
@@ -19,7 +19,7 @@ namespace xamarinJKH {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class AppResources {
@@ -399,6 +399,15 @@ namespace xamarinJKH {
         internal static string AppsPerformed {
             get {
                 return ResourceManager.GetString("AppsPerformed", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Не выбран тип заявки.
+        /// </summary>
+        internal static string AppTypeNotSelected {
+            get {
+                return ResourceManager.GetString("AppTypeNotSelected", resourceCulture);
             }
         }
         
